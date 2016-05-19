4dcd4552a0d79351420719098bff4079be29f043
mmochetti <martin.mochetti@gmail.com>
2016-05-19 12:56:25 -0300

    Fixing comments trailing spaces

--- src/main.js
+++ src/main.js
@@ -7,18 +7,18 @@ import 'bootstrap/less/bootstrap.less'
 Vue.use(VueRouter)
 
 // Create a router instance.
-// You can pass in additional options 
+// You can pass in additional options
 // here, but let's keep it simple for now.
 var router = new VueRouter()
 
 // Define some routes.
-// Each route should map to a component. 
-// The "component" can either be an actual component 
+// Each route should map to a component.
+// The "component" can either be an actual component
 // constructor created via Vue.extend(), or just a component options object.
 router.map(require('./routes').default)
 
 // Now we can start the app!
-// The router will create an instance of App and 
+// The router will create an instance of App and
 // mount to the element matching the selector #app.
 router.start(Vue.extend({
   components: {
