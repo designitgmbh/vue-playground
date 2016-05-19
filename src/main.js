df95e890bd4af65d2180873001434828e5eb0a1b
mmochetti <martin.mochetti@gmail.com>
2016-05-19 12:55:14 -0300

    Change how toggleSidebar is toggled

--- src/main.js
+++ src/main.js
@@ -7,28 +7,22 @@ import 'bootstrap/less/bootstrap.less'
 Vue.use(VueRouter)
 
 // Create a router instance.
-// You can pass in additional options here, but let's
-// keep it simple for now.
+// You can pass in additional options 
+// here, but let's keep it simple for now.
 var router = new VueRouter()
 
 // Define some routes.
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// Vue.extend(), or just a component options object.
-// We'll talk about nested routes later.
+// Each route should map to a component. 
+// The "component" can either be an actual component 
+// constructor created via Vue.extend(), or just a component options object.
 router.map(require('./routes').default)
 
 // Now we can start the app!
-// The router will create an instance of App and mount to
-// the element matching the selector #app.
+// The router will create an instance of App and 
+// mount to the element matching the selector #app.
 router.start(Vue.extend({
   components: {
     Sidebar
-  },
-  methods: {
-    toggleSidebar (evt) {
-      this.sidebarHidden = !this.sidebarHidden
-    }
   },
   data () {
     return {
