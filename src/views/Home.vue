68fe087816af52a4099cda1c8ea3b568d8b663b9
mmochetti <martin.mochetti@gmail.com>
2016-05-18 23:17:07 -0300

    Adding stylus support

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-    <img class="logo" src="../assets/logo.png">
+    <img class="logo" src="../assets/img/logo.png">
     <hello></hello>
     <p>
-      Welcome to your Vue.js app!
+      Welcome to your Vue.js playground!
     </p>
     <p>
       To get a better understanding of how this boilerplate works, check out
@@ -30,33 +30,8 @@ export default {
 }
 </script>
 
-<style>
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
+<style lang="stylus">
+  .logo
+    width: 100px
+    height: 100px
 </style>
